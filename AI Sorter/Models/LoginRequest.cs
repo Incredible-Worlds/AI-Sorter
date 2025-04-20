63168000c6cddce1ae86ba44d5dd89107d759fb9
Kamys <116730181+Kamysdev@users.noreply.github.com>
2025-04-20 22:05:45 +0700

    Add login page style (#9)
    Co-authored-by: Minory54 <olesya.sartina@gmail.com>

---
+++ AI Sorter/Models/LoginRequest.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AI_Sorter.Models
+{
+    public class LoginRequest
+    {
+        [Required(ErrorMessage = "Введите имя пользователя")]
+        public string Username { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Введите пароль")]
+        public string Password { get; set; } = string.Empty;
+    }
+}
