281206b63d229c044c746072d5860abfc7ce5e55
KamysDev <work.kamysdev@gmail.com>
2025-04-22 00:58:35 +0700

    Fix: some fixes

---
+++ AI Sorter/Models/RegisterRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace AI_Sorter.Models
+{
+	public class RegisterRequest
+	{
+		public string Login { get; set; }
+		public string Password { get; set; }
+		public string Role { get; set; }
+	}
+
+}
